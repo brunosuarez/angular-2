4fd3a1a84e86c371d81e818de7def1682352e3f3
bruno.soares <bruno.soares@db1.com.br>
2021-10-15 22:14:04 -0300

    subindo módulo 2 e 3

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,7 @@
-import { AuthService } from './../../core/auth.service';
-import { Component, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { PlatformDetectorService } from './../../core/platform-detector/platform-detector.service';
+import { AuthService } from '../../core/auth/auth.service';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 @Component({
@@ -9,11 +11,13 @@ import { Router } from '@angular/router';
 export class SignInComponent implements OnInit{
 
     loginForm: FormGroup;
+    @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
 
     constructor(
       private formBuilder: FormBuilder,
       private authService: AuthService,
-      private router: Router) {}
+      private router: Router,
+      private platformDetectorService: PlatformDetectorService) {}
 
     ngOnInit(): void {
       this.loginForm = this.formBuilder.group({
@@ -23,7 +27,6 @@ export class SignInComponent implements OnInit{
     }
 
     login(){
-
       const userName = this.loginForm.get('userName').value;
       const password = this.loginForm.get('password').value;
 
@@ -33,7 +36,11 @@ export class SignInComponent implements OnInit{
           () => this.router.navigate(['user', userName]),
           err => {
             console.log(err);
-            this.loginForm.reset()
+            this.loginForm.reset();
+            this.platformDetectorService.isPlatformBrowser() &&
+                this.userNameInput.nativeElement.focus();
+
+            alert('Invalid user name or password');
           }
         )
     }
